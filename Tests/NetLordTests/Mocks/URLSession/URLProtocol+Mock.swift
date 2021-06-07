efcbbd0241a0f4f0328273a81d5c9d2d30ffef14
Kakhi Kiknadze <kakhi.kiknadze@wandio.com>
2021-06-07 11:35:29 +0400

    Removed NetworkManager tests

---
+++ Tests/NetLordTests/Mocks/URLSession/URLProtocol+Mock.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kakhi Kiknadze on 07.06.21.
+//
+
+import Foundation
--- Tests/NetLordTests/NetworkManager/NetworkManagerTests.swift
+++ Tests/NetLordTests/NetworkManager/NetworkManagerTests.swift
@@ -18,23 +18,4 @@ final class NetworkManagerTests: XCTestCase {
         cancellables = []
     }
     
-    func testRequestAuthorization() {
-        let request = URLRequest(url: URL(string: "https://example.com")!)
-        let tokenValue = "ACCESS_TOKEN"
-        let authorizer: Authorizing = makeAuthorizer(fails: false, tokenValue: tokenValue)
-        let manager = NetworkManager(session: URLSession(configuration: .default), authorizer: authorizer)
-        manager.perform(
-            request: request,
-            responseType: Data.self,
-            decoder: nil,
-            retryCount: 0,
-            needsAuthorization: true
-        )
-        .sink { result in
-            guard case .failure = result else { return }
-            XCTFail("Expected Authorizer not to fail but it failed")
-        } receiveValue: { _ in }
-        .store(in: &cancellables)
-    }
-    
 }
